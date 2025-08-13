53c596b083f4ee04bb67c39842f3c5b55cc15ee7
joelmir <https://github.com/joelmirmanoel/pay-friends.git>
2025-08-13 05:33:21 -0300

    primeiro commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./_components/navbar/navbar.component";
+import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
+import { SecondaryButtonComponent } from "./_components/secondary-button/secondary-button.component";
+import { ItemCertificadoComponent } from "./_components/item-certificado/item-certificado.component";
+import { BaseUiComponent } from "./_components/base-ui/base-ui.component";
+import { CertificadosComponent } from "./pages/certificados/certificados.component";
+import { CertificadoFormComponent } from "./pages/certificado-form/certificado-form.component";
+import { CertificadoComponent } from "./pages/certificado/certificado.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, ItemCertificadoComponent, BaseUiComponent, CertificadosComponent, CertificadoFormComponent, CertificadoComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'gerador-certificado';
+  exibeNavbar: boolean = true;
+}
