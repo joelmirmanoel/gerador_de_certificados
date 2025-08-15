8677745ef65f4f502873180131316415b3aa1d0e
joelmir <https://github.com/joelmirmanoel/pay-friends.git>
2025-08-15 03:43:17 -0300

    Formulário finalizado

--- src/app/pages/certificado-form/certificado-form.component.ts
+++ src/app/pages/certificado-form/certificado-form.component.ts
@@ -1,17 +1,47 @@
 import { Component } from '@angular/core';
 import { PrimaryButtonComponent } from "../../_components/primary-button/primary-button.component";
 import { SecondaryButtonComponent } from "../../_components/secondary-button/secondary-button.component";
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Certificado } from '../../interfaces/certificado';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [PrimaryButtonComponent, SecondaryButtonComponent, FormsModule],
+  imports: [PrimaryButtonComponent, SecondaryButtonComponent, FormsModule, CommonModule],
   templateUrl: './certificado-form.component.html',
   styleUrl: './certificado-form.component.css'
 })
 export class CertificadoFormComponent {
-  nome: string = '';
-  atividade: string = '';
-  atividades: string[] = [];
 
+
+
+  certificado: Certificado = {
+    atividades: [],
+    nome: '',
+  }
+  atividade: string = '';
+
+
+  campoInvalido(control: NgModel) {
+    return control.invalid && control.touched
+  }
+
+  formValido() {
+    return this.certificado.atividades.length > 0 && this.certificado.nome.length > 0;
+  }
+
+  adicionarAtividade() {
+    this.certificado.atividades.push(this.atividade);
+    this.atividade = '';
+  }
+
+  excluirAtividade(index: number) {
+    this.certificado.atividades.splice(index, 1)
+  }
+
+  submit() {
+    if(!this.formValido()){
+      return
+    }
+  }
 }
--- src/app/pages/certificados/certificados.component.ts
+++ src/app/pages/certificados/certificados.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { ItemCertificadoComponent } from "../../_components/item-certificado/item-certificado.component";
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { SecondaryButtonComponent } from "../../_components/secondary-button/secondary-button.component";
 
 @Component({
   selector: 'app-certificados',
-  imports: [ItemCertificadoComponent],
+  imports: [ItemCertificadoComponent, SecondaryButtonComponent, RouterLink],
   templateUrl: './certificados.component.html',
   styleUrl: './certificados.component.css'
 })
